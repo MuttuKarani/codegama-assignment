a929fbb7789060da6e0de4e345a0c914d91a1096
MuttuKarani <muttukarani86@gmail.com>
2025-11-05 16:57:37 +0530

    nav bar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -46,3 +46,81 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+// import React, { useState } from "react";
+// import { Link } from "react-scroll";
+// import { Menu, X } from "lucide-react"; // Hamburger icons
+// import "../../scss/custom.scss";
+
+// const NavBar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => setIsOpen(!isOpen);
+
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-container">
+//         <div className="logo">
+//           <img src="/assets/LOGO.png" alt="Logo" />
+//         </div>
+
+//         {/* Hamburger icon for mobile */}
+//         <div className="hamburger" onClick={toggleMenu}>
+//           {isOpen ? <X size={28} /> : <Menu size={28} />}
+//         </div>
+
+//         <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+//           <li>
+//             <Link
+//               to="features"
+//               smooth={true}
+//               duration={600}
+//               offset={-70}
+//               onClick={() => setIsOpen(false)}
+//             >
+//               Features
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link
+//               to="resources"
+//               smooth={true}
+//               duration={600}
+//               offset={-70}
+//               onClick={() => setIsOpen(false)}
+//             >
+//               Resources
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link
+//               to="comparison"
+//               smooth={true}
+//               duration={600}
+//               offset={-70}
+//               onClick={() => setIsOpen(false)}
+//             >
+//               Comparison
+//             </Link>
+//           </li>
+
+//           <li>
+//             <Link
+//               to="blogs"
+//               smooth={true}
+//               duration={600}
+//               offset={-70}
+//               onClick={() => setIsOpen(false)}
+//             >
+//               Blogs
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
